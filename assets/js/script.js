556c1d1d4ca8552d24e6f65f3c16f331dbc101cb
A-A-ronMend0za <aaronmendoza.denver@gmail.com>
2024-11-25 13:21:54 -0700

    feat: card added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,19 @@ var dashboardEl = $("#dashboardEl");
 var footerEl = $("#footerEl");
 
 var startBtn = $("#startBtn");
+var cardEl = $("#cardEl");
 
 startBtn.click(function () {
   headerEl.attr("style", "flex-grow:0;");
   footerEl.attr("style", "flex-grow:0; animation-name: footer;");
-  dashboardEl.attr("style", "flex-grow: 1");
+  dashboardEl.attr("style", "display: flex; flex-grow: 1;");
   startBtn.attr(
     "style",
     "margin-bottom: 0; transition: 0.5s; transition-delay: 1s;"
   );
+
+  //   cardEl.attr(
+  //     "style",
+  //     "display: flex; width: var(--fs-4);height: var(--fs-5);"
+  //   );
 });
