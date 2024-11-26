00100d92e0990197315967b783e3a34b9771c2d5
A-A-ronMend0za <aaronmendoza.denver@gmail.com>
2024-11-26 01:11:37 -0700

    feat: unown add getRandomPokemon

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,14 @@ var pokeballBottomEl = $("#pokeballBottomEl");
 
 var cardEl = $("#cardEl");
 var cardfaceEl = $("#cardfaceEl");
+var unown = $("#unown");
 var nameEl = $("#nameEl");
 var hpEl = $("#hpEl");
 var pkmnImg = $("#pkmnImg");
 var detailRibbonEL = $("#detailRibbonEl");
 var moveNodeEl = $("#moveNodeEl");
 var baseMoveEl = $("#baseMoveEl");
-var attackEl = $("# attackEl");
+var attackEl = $("#attackEl");
 var specialMoveEl = $("#spacialMoveEl");
 var specialAttackEl = $("#specialAttackEl");
 var cardFooterEl = $("#cardFooterEl");
@@ -23,6 +24,10 @@ var cardBottomEl = $("#cardBottomEl");
 
 var pokemonOptionsEl = $("#pokemonOptionsEl");
 var cardOptionsEl = $("#cardOptionsEl");
+
+var currentPokemon = 0;
+var currentPokemonData = {};
+var currentPokemonType = "";
 
 startBtn.click(function () {
   headerEl.attr("style", "flex-grow:0;");
@@ -36,4 +41,33 @@ startBtn.click(function () {
     "style",
     "margin-top: 5rem; transition: 0.5s; transition-delay: 1s; height: 0;"
   );
+});
+
+function getPokemon(i) {
+  var requestUrl = "https://pokeapi.co/api/v2/pokemon/" + i + "/";
+  fetch(requestUrl)
+    .then(function (response) {
+      if (response.status === 404) {
+        console.log("404");
+      } else {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      currentPokemon = data.id;
+      currentPokemonData = data;
+      currentPokemonType = data.types[0].type.name;
+    });
+}
+
+function getRandomPokemon() {
+  var x = Math.floor(Math.random() * 1024) + 1;
+  getPokemon(x);
+  console.log(currentPokemonData.name);
+}
+
+unown.click(getRandomPokemon);
+
+cardOptionsEl.click(function () {
+  console.log(currentPokemonData);
 });
