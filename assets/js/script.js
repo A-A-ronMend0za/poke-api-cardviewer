5a5032aff3d38586b9674a178e408f83b66827eb
A-A-ronMend0za <aaronmendoza.denver@gmail.com>
2024-11-26 01:53:25 -0700

    feat: displayPokemon

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,16 @@ var pokeballBottomEl = $("#pokeballBottomEl");
 var cardEl = $("#cardEl");
 var cardfaceEl = $("#cardfaceEl");
 var unown = $("#unown");
+var titleEl = $("#titleEl");
 var nameEl = $("#nameEl");
 var hpEl = $("#hpEl");
 var pkmnImg = $("#pkmnImg");
 var detailRibbonEL = $("#detailRibbonEl");
 var moveNodeEl = $("#moveNodeEl");
 var baseMoveEl = $("#baseMoveEl");
+var specialNodeEl = $("#specialNodeEl");
 var attackEl = $("#attackEl");
-var specialMoveEl = $("#spacialMoveEl");
+var specialMoveEl = $("#specialMoveEl");
 var specialAttackEl = $("#specialAttackEl");
 var cardFooterEl = $("#cardFooterEl");
 var abilityEl = $("#abilityEl");
@@ -57,13 +59,80 @@ function getPokemon(i) {
       currentPokemon = data.id;
       currentPokemonData = data;
       currentPokemonType = data.types[0].type.name;
+      displayPokemon(data);
     });
 }
 
 function getRandomPokemon() {
   var x = Math.floor(Math.random() * 1024) + 1;
   getPokemon(x);
-  console.log(currentPokemonData.name);
+}
+
+function displayPokemon(data) {
+  cardEl.attr("class", "card");
+  cardfaceEl.attr("class", "cardface");
+  titleEl.css("display", "flex");
+  unown.css("display", "none");
+  pkmnImg.css("display", "flex");
+  detailRibbonEL.css("display", "flex");
+  moveNodeEl.css("display", "flex");
+  specialNodeEl.css("display", "flex");
+  cardFooterEl.css("display", "flex");
+  infoBoxEl.css("display", "flex");
+  cardBottomEl.css("display", "flex");
+
+  nameEl.text(
+    data.name
+      .split(" ")
+      .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+      .join(" ")
+  );
+  detailRibbonEL.text(
+    data.types[0].type.name
+      .split(" ")
+      .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+      .join(" ") +
+      " Pokemon. HT: " +
+      data.height / 10 +
+      " m, WT: " +
+      data.weight / 10 +
+      " kg."
+  );
+  hpEl.text(data.stats[0].base_stat + " HP");
+  var artworkUrl =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" +
+    data.id +
+    ".png";
+  pkmnImg.attr("src", artworkUrl);
+  baseMoveEl.text(
+    data.moves[0].move.name
+      .split(" ")
+      .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+      .join(" ")
+  );
+  attackEl.text(data.stats[1].base_stat);
+  specialMoveEl.text(
+    data.moves[1].move.name
+      .split(" ")
+      .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+      .join(" ")
+  );
+  specialAttackEl.text(data.stats[3].base_stat);
+  infoBoxEl.text(
+    " EXP. " +
+      data.base_experience +
+      "  Speed: " +
+      data.stats[5].base_stat +
+      "  #" +
+      data.id
+  );
+  abilityEl.text(
+    "Special Ability: " +
+      data.abilities[0].ability.name
+        .split(" ")
+        .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+        .join(" ")
+  );
 }
 
 unown.click(getRandomPokemon);
@@ -71,3 +140,5 @@ unown.click(getRandomPokemon);
 cardOptionsEl.click(function () {
   console.log(currentPokemonData);
 });
+
+pokemonOptionsEl.click(getRandomPokemon);
