f90dad6c84680bd88d45829f112a8b3c9ae960e8
A-A-ronMend0za <aaronmendoza.denver@gmail.com>
2024-11-26 01:58:31 -0700

    style: card background color match type

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,6 +133,102 @@ function displayPokemon(data) {
         .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
         .join(" ")
   );
+
+  if (currentPokemonType == "dark") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, black, rgb(37, 37, 37), black); color: white;"
+    );
+    detailRibbonEL.attr("style", "color: black");
+    imgContainerEl.attr("style", "color: black");
+  } else if (currentPokemonType == "fire") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, red, rgb(163, 0, 0), red)"
+    );
+  } else if (currentPokemonType == "electric") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, gold, yellow, gold)"
+    );
+  } else if (currentPokemonType == "grass") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, green, rgb(0, 114, 0), green)"
+    );
+  } else if (currentPokemonType == "bug") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, green, rgb(0, 249, 0), green"
+    );
+  } else if (currentPokemonType == "water") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, blue, rgb(48, 86, 255), blue)"
+    );
+  } else if (currentPokemonType == "ice") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, blue, white, blue)"
+    );
+  } else if (currentPokemonType == "psychic") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, purple, rgb(225, 0, 225), purple)"
+    );
+  } else if (currentPokemonType == "ghost") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg,rgb(102, 0, 102),rgb(128, 0, 188),rgb(93, 0, 93))"
+    );
+  } else if (currentPokemonType == "poison") {
+    cardEl.attr(
+      "style",
+      " background-image: linear-gradient(135deg,rgb(54, 0, 124),rgb(129, 33, 255),rgb(54, 0, 124))"
+    );
+  } else if (currentPokemonType == "steel") {
+    cardEl.attr(
+      "style",
+      " background-image: linear-gradient(135deg,rgb(80, 80, 80),white,rgb(80, 80, 80))"
+    );
+  } else if (currentPokemonType == "normal") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, rgb(228, 228, 228),rgb(186, 186, 186), rgb(228, 228, 228))"
+    );
+  } else if (currentPokemonType == "flying") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg, rgb(228, 228, 228),rgb(186, 186, 186), rgb(228, 228, 228))"
+    );
+  } else if (currentPokemonType == "ground") {
+    cardEl.attr(
+      "style",
+      "background-image: linear-gradient(135deg,rgb(146, 95, 0),orange,rgb(146, 95, 0))"
+    );
+  } else if (currentPokemonType == "rock") {
+    cardEl.attr(
+      "style",
+      " background-image: linear-gradient(135deg,rgb(146, 73, 0),orange,rgb(146, 73, 0))"
+    );
+  } else if (currentPokemonType == "fairy") {
+    cardEl.attr(
+      "style",
+      "  background-image: linear-gradient(135deg,rgb(255, 120, 255),pink,rgb(255, 120, 255))"
+    );
+  } else if (currentPokemonType == "dragon") {
+    cardEl.attr(
+      "style",
+      " background-image: linear-gradient(135deg,rgb(146, 73, 0),orange,rgb(146, 73, 0))"
+    );
+  } else if (currentPokemonType == "fighting") {
+    cardEl.attr(
+      "style",
+      " background-image: linear-gradient(135deg,rgb(146, 73, 0),orange,rgb(146, 73, 0))"
+    );
+  } else {
+    cardEl.attr("style", "background-color: magenta");
+  }
 }
 
 unown.click(getRandomPokemon);
